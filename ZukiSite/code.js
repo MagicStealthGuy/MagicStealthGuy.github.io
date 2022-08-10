8b6935fa7656ff239c5c5ddac188f90ad346bc30
MagicStealthGuy <magicstealthguy@gmail.com>
2022-08-10 19:43:15 -0400

    Slider 3.2.0
    Romance Edition

--- ZukiSite/code.js
+++ ZukiSite/code.js
@@ -26,3 +26,32 @@ function updateSlider(passObj) {
   nextObj.find("span.bar > span").css("width", percentage + "%");
   nextObj.find("span.bar-btn > span").text(value);
 };
+
+$("document").ready(function() {
+  $('.rslider').each(function() {
+    $(this).rangerslider();
+  });
+});
+$.fn.rangerslider = function(options) {
+  var obj = this;
+  var defautValue = obj.attr("value");
+  obj.wrap("<span class='range-rslider'></span>");
+  obj.after("<span class='slider-container'><span class='bar'><span></span></span><span class='bar-btn'><span>0</span></span></span>");
+  obj.attr("oninput", "updateSlider(this)");
+  updateSlider(this);
+  return obj;
+};
+
+
+function updateSlider(passObj) {
+  var obj = $(passObj);
+  var value = obj.val();
+  var min = obj.attr("min");
+  var max = obj.attr("max");
+  var range = Math.round(max - min);
+  var percentage = Math.round((value - min) * 100 / range);
+  var nextObj = obj.next();
+  nextObj.find("span.bar-btn").css("left", percentage + "%");
+  nextObj.find("span.bar > span").css("width", percentage + "%");
+  nextObj.find("span.bar-btn > span").text(value);
+};
